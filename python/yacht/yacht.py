27979bb27c45257b58c1debcb722d9d2abf3ef23
tchartchke <chartai.kl@gmail.com>
2019-08-21 16:30:33 -0500

    Completed yacht for python. Not yet reviewed with Lucas

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,3 +1,4 @@
+import operator
 """
 This exercise stub and the test suite contain several enumerated constants.
 
@@ -14,19 +15,77 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+ONES = 1
+TWOS = 2
+THREES = 3
+FOURS = 4
+FIVES = 5
+SIXES = 6
+FULL_HOUSE = 7
+FOUR_OF_A_KIND = 8
+LITTLE_STRAIGHT = 9
+BIG_STRAIGHT = 10
+CHOICE = 11
+YACHT = 12
+
+#scores based off of game type
+def num(dice, n):
+  score = 0
+  for d in dice:
+    if d == n:
+      score += n
+  return score
+
+def full(rolls, dice):
+  if len(rolls) == 2:
+    l = rolls.values()
+    if all(elem in l  for elem in [2, 3]):
+      return sum(dice)
+  return 0
+ 
+def kind(rolls):
+  m = max(rolls, key=lambda key: rolls[key])
+  if rolls[m] > 3:
+    return m*4
+  return 0
+ 
+def l_straight(dice):
+  if all(elem in dice  for elem in [1, 2, 3, 4, 5]):
+      return 30
+  return 0
+ 
+def b_straight(dice):
+  if all(elem in dice  for elem in [2, 3, 4, 5, 6]):
+      return 30
+  return 0
+ 
+def choice(dice):
+  return sum(dice)
+ 
+def yacht(dice):
+  if dice.count(dice[0]) == 5:
+      return 50
+  return 0
 
 
 def score(dice, category):
-    pass
+  # map ==> roll : count 
+  rolls = {}
+  for d in dice:
+    if d in rolls:
+      rolls[d] += 1
+    else:
+      rolls[d] = 1
+
+  switcher = {
+    category: num(dice, category),
+    7: full(rolls, dice),
+    8: kind(rolls),
+    9: l_straight(dice),
+    10: b_straight(dice),
+    11: choice(dice),
+    12: yacht(dice)
+  }
+  
+  func = switcher.get(category, lambda: "Invalid Game")
+  return func
