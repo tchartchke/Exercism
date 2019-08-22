625b17dfc4326161fca26f3e7c22c742b4277f60
tchartchke <chartai.kl@gmail.com>
2019-08-21 22:27:36 -0500

    completed isbn_verifier

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,21 @@
 def is_valid(isbn):
-    pass
+  isbn = isbn.translate( { ord(c):None for c in '- ' })
+  if not isbn[:9].isnumeric():
+    return False
+    
+  if len(isbn) != 10:
+    return False
+
+  formula = 0
+  multiplier = 10
+  for i in range(0,9):
+    formula += multiplier*int(isbn[i])
+    multiplier -= 1
+
+  if isbn[9].isnumeric():
+    return (formula+int(isbn[9]))%11==0
+
+  if isbn[9] == 'X':
+    return (formula+10)%11==0
+
+  return False
