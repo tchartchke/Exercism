fcd38a8aa447f99a05d047a0cbb8b3d450d0f359
tchartchke <chartai.kl@gmail.com>
2019-08-21 20:58:15 -0500

    took out comments

--- python/series/series.py
+++ python/series/series.py
@@ -5,15 +5,10 @@ def slices(series, length):
   if length < 1:
     raise ValueError("not valid length")
 
-  print(series)
-  print(type(series))
-  print(len(series))
   strings = []
   i = 0
   while i+length <= len(series):
-    print(i+length)
     strings.append(series[i:i+length])
-    print(series[i:i+length])
     i+=1
 
   return strings
