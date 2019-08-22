6b81bb5ddb13688e8197cd419fc5389f9950dba8
tchartchke <chartai.kl@gmail.com>
2019-08-21 20:56:24 -0500

    completed series in python

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,19 @@
 def slices(series, length):
-    pass
+  series = str(series)
+  if len(series) < length:
+    raise ValueError("string not long enough")
+  if length < 1:
+    raise ValueError("not valid length")
+
+  print(series)
+  print(type(series))
+  print(len(series))
+  strings = []
+  i = 0
+  while i+length <= len(series):
+    print(i+length)
+    strings.append(series[i:i+length])
+    print(series[i:i+length])
+    i+=1
+
+  return strings
