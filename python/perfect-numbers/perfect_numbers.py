860dd536c3cb6c0b46af52d700f770689de75c2c
tchartchke <chartai.kl@gmail.com>
2019-08-23 17:25:05 -0500

    partly thru perfect numbers. haven't trouble with squares

---
+++ python/perfect-numbers/perfect_numbers.py
@@ -0,0 +1,26 @@
+def classify(number):
+  if number <= 0:
+    raise ValueError("Invalid number")
+  if number == 1:
+    return "deficient"
+
+  num = number
+  s = 1
+  div = 2
+
+  while num > div:
+    if number % div == 0:
+      num = number//div
+      s += (div + num)
+    
+    div += 1
+    
+
+
+  if s == number:
+    return "perfect"
+
+  if s > number:
+    return "abundant"
+
+  return "deficient"
