7558d00777da78759d7e6fe6648204ef228ca3a4
tchartchke <chartai.kl@gmail.com>
2019-08-23 20:20:43 -0500

    completed allergies. Still don't understand @property

---
+++ python/allergies/allergies.py
@@ -0,0 +1,23 @@
+class Allergies(object):
+  __a = ['cats', 'pollen', 'chocolate', 'tomatoes', 'strawberries', 'shellfish', 'peanuts', 'eggs']
+  
+  def __init__(self, score):
+    # makes all the 1010101s
+    b = bin(score)[2:]
+    if len(b) > 8:
+      b = b[(len(b)-8):len(b)]
+    self.score = '{:08}'.format(int(b))
+
+    self.allergies = self.lst
+
+  def allergic_to(self, item):
+    return item in self.allergies
+
+  @property
+  def lst(self):
+    allergies = []
+    for i in range(len(self.score)-1, -1, -1):
+      if self.score[i] == '1':
+        allergies.append(self.__a[i])
+    self.allergies = allergies
+    return self.allergies
