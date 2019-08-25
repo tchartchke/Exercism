975a7eaaf358c1e91c1ca6ac3d5ab121d6d51677
tchartchke <chartai.kl@gmail.com>
2019-08-25 09:47:06 -0500

    changed comments in error handling. prime factors made slightly more efficient

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -1,7 +1,6 @@
 def handle_error_by_throwing_exception():
   raise Exception("This is an exception")
 
-
 def handle_error_by_returning_none(input_data):
   if input_data.isnumeric():
     return int(input_data)
@@ -11,14 +10,15 @@ def handle_error_by_returning_tuple(input_data):
   if input_data.isnumeric():
     return True, int(input_data)
   return False, () 
-# fail can return anytihng with the false becuse dont matter
+# fail can return anything with the False because doesn't matter
 
 def filelike_objects_are_closed_on_exception(filelike_object):
   with filelike_object as f:
     f.do_something()
   # with = try-finally. Runs enter and exit automatically.
-  # try: 
-  #   filelike_object.open()
-  #   filelike_object.do_something
-  # finally :
-    # filelike_object.close()
+  # Equivalent to
+    # try: 
+    #   filelike_object.open()
+    #   filelike_object.do_something
+    # finally :
+      # filelike_object.close()
--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,13 +1,13 @@
 def factors(value):
   all_factors = []
   
+  curr_divisor = 2
   while value > 1:
     for div in range(2,value+1):
       if value % div == 0:
         all_factors.append(div)
         value //= div
         break
+      else:
+        curr_divisor += 1
   return all_factors
-
-# keep a current value that can be incremented because if it's not d
-# divisible by 2, it'll never be divisible by 2 aagain
