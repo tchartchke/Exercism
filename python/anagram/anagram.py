af9a1bbce4b8eced63f21f047feb4a7b2ca78244
tchartchke <chartai.kl@gmail.com>
2019-08-25 09:35:47 -0500

    reworked anagram and triangle with lucas suggestions

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,45 +1,10 @@
-from itertools import permutations 
-
 def find_anagrams(word, candidates):
-  word = word.lower()
-  perm = list(permutations(word))
-  p_words = []
-  for p in perm:
-    string = ''.join(p)
-    p_words.append(string)
-
   found = []
   for c in candidates:
-    if c.casefold() == word:
-      continue
-    if len(c) != len(word):
-      continue
-    if c.casefold() in p_words:
-      if c not in found:
+    if sorted(word.lower()) == sorted(c.lower()):
+      if c.lower() == word.casefold():
+        continue
+      if not c in found:
         found.append(c)
-
   return found
-
-  # def find_anagrams(word, candidates):
-  #   return[candidate for candidate in candidates 
-  #          if (word.lower() != candidate.lower() and sorted(candidate.lower()) == sorted(word.lower()))]
-
-
-# for loop looks for iterators. itertools returns iterator
-  # p_words = []
-  # for p in permutations(word):
-  #   string = ''.join(p)
-  #   p_words.append(string)
-
-
-# map{lambda}instead of first forloop
-
-# check is uppwer or lower actually changes original string
-
-# set to list conversion
-# set and map have constant look up time
-
-
-
-# check if sets are equal if saved in locations. can use set operations, set equality
-# can python do multisets
+      
--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,41 +1,20 @@
 def equilateral(sides):
   if not triangle_inequality(sides):
     return False
-  if sides[0] == sides[1]:
-    if sides[1] == sides[2]:
-      return True
-  return False
-  # can compress to one line
+  return sides[0] == sides[1] and sides[1] == sides[2]
 
 def isosceles(sides):
   if not triangle_inequality(sides):
     return False
-  if sides[0] == sides[1]:
-    return True
-  if sides[1] == sides[2]:
-    return True
-  if sides[0] == sides[2]:
-    return True
-  return False
-  # can compress to one line
+  return sides[0] == sides[1] or sides[1] == sides[2] or sides[0] == sides[2]
 
 def scalene(sides):
-  if not triangle_inequality(sides):
-    return False
-  return not isosceles(sides)
-
-# return triangle_inequality(sides) and not isosceles(sides)
+  return triangle_inequality(sides) and not isosceles(sides)
 
 def triangle_inequality(sides):
-  for i in range(len(sides)):
-    if sides[i] <= 0:
+  s = sides
+  for i in range(len(s)):
+    if s[i] <= 0:
       return False
-  sides.sort()
-  return sides[0]+sides[1] > sides[2]
-
-
-
-  # sort will change original data for ... complex objects. Passed pointer to data instead of actual data
-
-
-  # can just return boolean
+  s.sort()
+  return s[0]+s[1] > s[2]
